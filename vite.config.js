35de46b6ae452ed34aa4f2da3ef8e97333531424
Ruben35 <ruben111416@gmail.com>
2022-09-13 21:54:20 -0500

    ⬇️ Vite downgrade in order to use svg

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from '@honkhonk/vite-plugin-svgr'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
 		},
 	},
-	plugins: [react()],
-});
+	plugins: [react(), svgr()],
+})
