bd27e7ab97768e6e7488593e092eb55790db5cce
Ruben35 <ruben111416@gmail.com>
2022-09-29 10:27:59 -0500

    ✨ Added RadioButtons

--- src/components/Forms/RadioOption.jsx
+++ src/components/Forms/RadioOption.jsx
@@ -1,5 +1,35 @@
-const RadioOption = () => {
-	return <div>RadioOption</div>
+import PropTypes from 'prop-types'
+
+const RadioOption = ({ value, name, defaultChecked, color }) => {
+	return (
+		<label>
+			<input
+				type='radio'
+				value={value}
+				name={name}
+				defaultChecked={defaultChecked}
+			/>
+			<span
+				className={`customRadio ${
+					color === 'primary'
+						? 'customRadio--primary'
+						: color === 'secondary' && 'customRadio--secondary'
+				}`}
+			></span>
+			<span>{value}</span>
+		</label>
+	)
 }
 
+RadioOption.propTypes = {
+	value: PropTypes.string,
+	name: PropTypes.string,
+	defaultChecked: PropTypes.bool,
+	color: PropTypes.oneOf(['neutral', 'primary', 'secondary']),
+}
+
+RadioOption.defaultProps = {
+	value: 'Radio',
+	color: 'neutral',
+}
 export default RadioOption
--- src/components/Forms/RadioOptionGroup.jsx
+++ src/components/Forms/RadioOptionGroup.jsx
@@ -1,5 +1,46 @@
-const OptionGroup = () => {
-	return <div>OptionGroup</div>
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RadioOptionGroup = ({ label, children, name, value, onChange }) => {
+	return (
+		<div
+			className='radioOptionGroup'
+			onChange={(e) => onChange(e.target.value)}
+		>
+			<span>{label}</span>
+			{React.Children.map(children, (child) => {
+				if (React.isValidElement(child)) {
+					if (child.props.value === value) {
+						const defaultChecked = true
+						return React.cloneElement(child, { name, defaultChecked })
+					}
+					return React.cloneElement(child, { name })
+				}
+			})}
+		</div>
+	)
 }
 
-export default OptionGroup
+RadioOptionGroup.propTypes = {
+	label: PropTypes.string,
+	name: PropTypes.string,
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+	children: (props, propName) => {
+		let error
+		const prop = props[propName]
+		React.Children.forEach(prop, (child) => {
+			if (child.type.name !== 'RadioOption')
+				error = new Error(
+					`<RadioOptionGroup> only accepts children of type <RadioOption>".`
+				)
+		})
+		return error
+	},
+}
+
+RadioOptionGroup.defaultProps = {
+	label: 'Label',
+}
+
+export default RadioOptionGroup
