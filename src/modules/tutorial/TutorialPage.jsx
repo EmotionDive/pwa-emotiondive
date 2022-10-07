f8be3eb354ad1a949169e29ec551568797cbd610
Ruben35 <ruben111416@gmail.com>
2022-10-06 23:03:44 -0500

    ✨ Adding first slide of tutorial

--- src/modules/tutorial/TutorialPage.jsx
+++ src/modules/tutorial/TutorialPage.jsx
@@ -1,7 +1,19 @@
 import AnimatedGradient from '../../components/Gradients/AnimatedGradient'
+import logo from '../../assets/images/logos/LogoHorizontal.png'
 
 const TutorialPage = () => {
-	return <AnimatedGradient>as</AnimatedGradient>
+	return (
+		<AnimatedGradient>
+			<div className='tutorialPage'>
+				<main>
+					<span>¡Hola Sarah!</span>
+				</main>
+				<footer>
+					<img src={logo} alt='EmotionDive'></img>
+				</footer>
+			</div>
+		</AnimatedGradient>
+	)
 }
 
 export default TutorialPage
