a2571d1cb08f6fadb4c1ee041fa96e25466a794b
Ruben35 <ruben111416@gmail.com>
2022-12-01 22:11:17 -0600

    ✨ Activity III of SelfKnowledge Complete

--- src/modules/activities/TCC/SelfKnowledge/DescribeSituationandEmotionAssigmentActivity.jsx
+++ src/modules/activities/TCC/SelfKnowledge/DescribeSituationandEmotionAssigmentActivity.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import { ActivitiesLocalizationBar } from '../../../../components/LocalizationBar'
+import {
+	LargeButtonModal,
+	ModalAction,
+	ModalProvider,
+} from '../../../../components/Modal'
+import { TextArea } from '../../../../components/Forms'
+import data from './data/DescribeSituationAndEmotionAssignmentData.json'
+import { EmotionButton } from './components'
+import { useState } from 'react'
+
+const emotionsMATEA = ['scare', 'happiness', 'sad', 'angry', 'love']
 
 const DescribeSituationandEmotionAssigmentActivity = () => {
-	return <div>DescribeSituationandEmotionAssigmentActivity</div>
+	const [repetition] = useState(5)
+	const [selectedEmotions, setSelectedEmotions] = useState([])
+	const [situation, setSituation] = useState('')
+	const [reason, setReason] = useState('')
+
+	const handleEmotion = (emotion) => {
+		const emotionsCopy = [...selectedEmotions]
+		const alreadyChoosed = emotionsCopy.indexOf(emotion)
+		if (alreadyChoosed !== -1) emotionsCopy.splice(alreadyChoosed, 1)
+		else emotionsCopy.push(emotion)
+		setSelectedEmotions(emotionsCopy)
+	}
+
+	return (
+		<div className='SelfKnowledgeActivity'>
+			<ModalProvider>
+				<ActivitiesLocalizationBar title={data.title} />
+				<main className='emotionalAssignmentActivity'>
+					<span className='systemText__instruction'>{data.instructions}</span>
+					<div className='question'>
+						<span className='question__label'>Situación</span>
+						<TextArea
+							label=''
+							placeholder='Redacta aquí una situación que estas viviendo o viviste...'
+							value={situation}
+							onChange={(e) => setSituation(e.target.value)}
+						/>
+						<span className='question__label'>
+							¿Qué sientes/sentiste en esta situación?
+						</span>
+						<div className='EmotionsButtons'>
+							{emotionsMATEA.map((emotion, index) => (
+								<EmotionButton
+									emotion={emotion}
+									onClick={() => handleEmotion(emotion)}
+									key={index}
+									active={selectedEmotions.includes(emotion)}
+								/>
+							))}
+						</div>
+						<span className='question__label'>
+							¿Porqué crees que sentiste estas emociones?
+						</span>
+						<TextArea
+							label=''
+							placeholder='Redacta aquí tu respuesta...'
+							value={reason}
+							onChange={(e) => setReason(e.target.value)}
+						/>
+					</div>
+					<LargeButtonModal
+						title={'¡Terminaste de asignar tus propias emociones!'}
+						info={repetition < 5 ? data.finalPartial : data.finalComplete}
+						variant='confirmation'
+						buttonLabels={['Okey']}
+						exitOnClickOut={false}
+						onConfirmationCallback={() => {
+							console.log('Finish')
+						}}
+						disabled={
+							selectedEmotions.length === 0 ||
+							situation.length < 20 ||
+							reason.length < 20
+						}
+					>
+						Terminar
+					</LargeButtonModal>
+				</main>
+				<ModalAction />
+			</ModalProvider>
+		</div>
+	)
 }
 
 export default DescribeSituationandEmotionAssigmentActivity
